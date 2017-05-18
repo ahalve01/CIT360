416a9040bb7f26dcc7d79fb0a517fad4e9e1cb84
Halverson <Halverson@Halverson-Desktop>
2017-05-17 21:20:28 -0400

    Update

--- src/simpleCalculator/TheMath.java
+++
@@ -1,6 +0,0 @@
-package simpleCalculator;
-
-public interface TheMath {
-	void execute(Integer n1, Integer n2);
-
-}
--- src/television/YourShow.java
+++
@@ -1,8 +0,0 @@
-package television;
-
-public interface YourShow{
-	void execute (String tv1, String tv2, String tv3, String tv4);
-
-	void execute(String command);
-
-}
