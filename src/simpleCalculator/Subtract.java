a8910b27f238f594f4f6994cc7d09d80ad28a476
Halverson <Halverson@Halverson-Desktop>
2017-05-17 20:33:43 -0400

    ACP Uploads

---
+++ src/simpleCalculator/Subtract.java
@@ -0,0 +1,10 @@
+package simpleCalculator;
+
+public class Subtract implements TheMath {
+	
+	public void execute(Integer n1, Integer n2){
+	Integer sub = n1 - n2;
+	System.out.println(sub);	
+	}
+	
+}
---
+++ src/television/YourShow.java
@@ -0,0 +1,8 @@
+package television;
+
+public interface YourShow{
+	void execute (String tv1, String tv2, String tv3, String tv4);
+
+	void execute(String command);
+
+}
